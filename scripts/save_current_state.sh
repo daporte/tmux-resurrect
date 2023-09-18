9b37799395260c15f2c09bcbd00bfb5cf92a2773
David Portes <daporte21@gmail.com>
2023-09-18 14:36:20 +0200

    add capture_pane_contents && preserve timestamp when copying over suspended session files

--- scripts/save_current_state.sh
+++ scripts/save_current_state.sh
@@ -4,6 +4,7 @@ source $HOME/projects/tmux_resurrect-worktree/master/scripts/save.sh
 cd $HOME/projects/tmux_resurrect-worktree/master/savefiles
 dump_panes
 dump_windows
-cp -nr $SAVES_DIR/last/* $SAVE_DIR
+capture_pane_contents
+cp -nrp $SAVES_DIR/last/* $SAVE_DIR
 ln -nsf $SAVE_DIR last
 
